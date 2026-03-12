750de6e602488d5fff8a52bba54a6ee50841c130
md mohosin <mdmohosin.web@gmail.com>
2026-03-13 00:29:52 +0600

    create skills and footer

--- src/main.js
+++ src/main.js
@@ -1,24 +1,30 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+document.getElementById("year").innerText = new Date().getFullYear()
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+
+
+const menuIcon = document.getElementById('menuIcon');
+const sidebar = document.getElementById("sidebar")
+const closeSidebar = document.getElementById("close-sidebar")
+const links = sidebar.querySelectorAll("a")
+
+
+
+
+menuIcon.addEventListener("click", () => {
+    sidebar.classList.add("active")
+})
+
+
+closeSidebar.addEventListener("click", () => {
+    sidebar.classList.remove("active")
+})
+
+
+
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        sidebar.classList.remove("active");
+    });
+});
